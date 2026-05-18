3abfd957a529833ec68ffdbd9e6a46b6b8b8bde9
Swastikad4 <swastikadey445@gmail.com>
2026-05-18 16:02:25 +0530

    Added ledger backend features

---
+++ src/models/transaction.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+
+const transactionSchema = new mongoose.Schema({
+    FromAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [ true, "Transaction must be associated with a from account" ],
+        index: true
+    },
+    ToAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [ true, "Transaction must be associated with a to account" ],
+        index: true
+    },
+    status:{
+        type: String,
+        enum:{
+            values: [ "PENDING", "COMPLETED", "FAILED", "REVERSED" ], 
+            message: "Status can be either PENDING, COMPLETED, FAILED or REVERSED",
+
+        },
+        default: "PENDING"
+    },
+    amount: {
+        type: Number,
+        required: [ true, "Amount is required for creating a transaction" ],
+        min: [0, "Transaction amount cannot be negative" ]
+    },
+    idempotencyKey: {
+        type: String,
+        required: [ true, "idempotency Key is required for creating a transaction" ],
+        index: true,
+        unique: true
+    }
+},{
+    timestamps: true
+})
+
+const transactionModel = mongoose.model("transaction", transactionSchema)
+
+module.exports=transactionModel
