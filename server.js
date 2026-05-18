b8ccd499266162bf68998670fb7c5766310a0197
Swastikad4 <swastikadey445@gmail.com>
2026-05-18 16:22:56 +0530

    fix dynamic port for Render

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-const app = require("./src/app")
-const connectToDB = require("./src/config/db")
+const app = require("./src/app");
+const connectToDB = require("./src/config/db");
 
+// 1. Define the port to use Render's dynamic port OR your local port 3000
+const PORT = process.env.PORT || 3000;
 
+connectToDB();
 
-connectToDB()
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
+// 2. Use the PORT variable here
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
